8f7ce9702e2f8e8b13a07812a611ee12f779b3d9
Bolek <103572991+BoloMasta@users.noreply.github.com>
2022-07-05 17:19:19 +0200

    main

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,6 +1,11 @@
 const hamburger = document.querySelector(".hamburger");
 const mobileMenu = document.querySelector(".hamburger__menu");
+const closeMenu = document.querySelector(".hamburger__close-btn");
+
+closeMenu.addEventListener("click", () => {
+  mobileMenu.classList.remove("hamburger__menu--active");
+});
 
 hamburger.addEventListener("click", () => {
-    
-})
+  mobileMenu.classList.add("hamburger__menu--active");
+});
